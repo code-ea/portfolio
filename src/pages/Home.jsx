a938e46acec0db9ddf1e7ca6e9f0ff55976686c9
Nitin Singh <borntocode.ea@gmail.com>
2025-02-01 01:31:58 +0530

    underdev banner

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,23 @@ import { Link } from 'react-router-dom';
 const Home = () => {
   return (
     <div style={{ textAlign: 'center', padding: '50px' }}>
+      {/* Development Banner */}
+      <div
+        style={{
+          backgroundColor: '#f8d7da',
+          color: '#721c24',
+          padding: '10px 0',
+          marginBottom: '20px',
+          fontWeight: 'bold',
+          fontSize: '16px',
+          borderRadius: '5px',
+          border: '1px solid #f5c6cb',
+        }}
+      >
+        🚧 Website Under Development 🚧
+      </div>
+
+      {/* Main Content */}
       <h1>Hello, I'm Nitin</h1>
       <p>Welcome to my portfolio</p>
       <p>
